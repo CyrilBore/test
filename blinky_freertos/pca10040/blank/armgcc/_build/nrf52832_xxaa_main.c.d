7976e26817613942446616209913eee6fde67fad
CyrilBore <cybore@orange.fr>
2017-02-03 21:14:59 +0100

    compilation

---
+++ blinky_freertos/pca10040/blank/armgcc/_build/nrf52832_xxaa_main.c.d
@@ -0,0 +1,238 @@
+_build/nrf52832_xxaa_main.c.o: ../../../main.c \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdbool.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/features.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../../../external/freertos/source/include/FreeRTOS.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stddef.h \
+ ../../../config/blinky_FreeRTOS_pca10040/FreeRTOSConfig.h \
+ ../../../../../../components/device/nrf.h \
+ ../../../../../../components/device/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../components/toolchain/system_nrf52.h \
+ ../../../../../../components/device/nrf52_bitfields.h \
+ ../../../../../../components/device/nrf51_to_nrf52.h \
+ ../../../../../../components/device/nrf52_name_change.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdio.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_ansi.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/newlib.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/config.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/cdefs.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdarg.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/reent.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_ansi.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_types.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/_types.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/lock.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/types.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/types.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/stdio.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../config/sdk_config.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_error_weak.h \
+ ../../../../../../external/freertos/source/include/projdefs.h \
+ ../../../../../../external/freertos/source/include/portable.h \
+ ../../../../../../external/freertos/source/include/deprecated_definitions.h \
+ ../../../../../../external/freertos/portable/GCC/nrf52/portmacro.h \
+ ../../../../../../external/freertos/portable/CMSIS/nrf52/portmacro_cmsis.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../external/freertos/source/include/mpu_wrappers.h \
+ ../../../../../../external/freertos/source/include/task.h \
+ ../../../../../../external/freertos/source/include/list.h \
+ ../../../../../../external/freertos/source/include/timers.h \
+ ../../../../../../external/freertos/source/include/task.h \
+ ../../../../../../components/libraries/bsp/bsp.h \
+ ../../../../../../components/boards/boards.h \
+ ../../../../../../components/drivers_nrf/hal/nrf_gpio.h \
+ ../../../../../../components/drivers_nrf/hal/nrf_peripherals.h \
+ ../../../../../../components/device/nrf52832_peripherals.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdlib.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/stdlib.h \
+ ../../../../../../components/boards/pca10040.h \
+ ../../../../../../components/libraries/button/app_button.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/drivers_nrf/clock/nrf_drv_clock.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/drivers_nrf/hal/nrf_clock.h \
+ ../../../../../../components/drivers_nrf/common/nrf_drv_common.h \
+ ../../../../../../components/libraries/util/sdk_common.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/string.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/string.h \
+ ../../../../../../components/libraries/util/sdk_os.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../components/libraries/util/sdk_macros.h
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdbool.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/features.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_newlib_version.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../../../external/freertos/source/include/FreeRTOS.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stddef.h:
+
+../../../config/blinky_FreeRTOS_pca10040/FreeRTOSConfig.h:
+
+../../../../../../components/device/nrf.h:
+
+../../../../../../components/device/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../components/toolchain/system_nrf52.h:
+
+../../../../../../components/device/nrf52_bitfields.h:
+
+../../../../../../components/device/nrf51_to_nrf52.h:
+
+../../../../../../components/device/nrf52_name_change.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/nrf_assert.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdio.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_ansi.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/newlib.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/config.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/ieeefp.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/cdefs.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdarg.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/reent.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_ansi.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_types.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/_types.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/lock.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/types.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/types.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/stdio.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+
+../config/sdk_config.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/app_error_weak.h:
+
+../../../../../../external/freertos/source/include/projdefs.h:
+
+../../../../../../external/freertos/source/include/portable.h:
+
+../../../../../../external/freertos/source/include/deprecated_definitions.h:
+
+../../../../../../external/freertos/portable/GCC/nrf52/portmacro.h:
+
+../../../../../../external/freertos/portable/CMSIS/nrf52/portmacro_cmsis.h:
+
+../../../../../../components/libraries/util/app_util.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../external/freertos/source/include/mpu_wrappers.h:
+
+../../../../../../external/freertos/source/include/task.h:
+
+../../../../../../external/freertos/source/include/list.h:
+
+../../../../../../external/freertos/source/include/timers.h:
+
+../../../../../../external/freertos/source/include/task.h:
+
+../../../../../../components/libraries/bsp/bsp.h:
+
+../../../../../../components/boards/boards.h:
+
+../../../../../../components/drivers_nrf/hal/nrf_gpio.h:
+
+../../../../../../components/drivers_nrf/hal/nrf_peripherals.h:
+
+../../../../../../components/device/nrf52832_peripherals.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdlib.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/stdlib.h:
+
+../../../../../../components/boards/pca10040.h:
+
+../../../../../../components/libraries/button/app_button.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/drivers_nrf/clock/nrf_drv_clock.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/drivers_nrf/hal/nrf_clock.h:
+
+../../../../../../components/drivers_nrf/common/nrf_drv_common.h:
+
+../../../../../../components/libraries/util/sdk_common.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/string.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/string.h:
+
+../../../../../../components/libraries/util/sdk_os.h:
+
+../../../../../../components/libraries/util/app_util.h:
+
+../../../../../../components/libraries/util/sdk_macros.h:
---
+++ blinky_freertos/pca10040/blank/armgcc/_build/nrf52832_xxaa_timers.c.d
@@ -0,0 +1,185 @@
+_build/nrf52832_xxaa_timers.c.o: \
+ ../../../../../../external/freertos/source/timers.c \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdlib.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_ansi.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/newlib.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/config.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/features.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stddef.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/reent.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_ansi.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_types.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/_types.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/lock.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/cdefs.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/stdlib.h \
+ ../../../../../../external/freertos/source/include/FreeRTOS.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_stdint.h \
+ ../../../config/blinky_FreeRTOS_pca10040/FreeRTOSConfig.h \
+ ../../../../../../components/device/nrf.h \
+ ../../../../../../components/device/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../components/toolchain/system_nrf52.h \
+ ../../../../../../components/device/nrf52_bitfields.h \
+ ../../../../../../components/device/nrf51_to_nrf52.h \
+ ../../../../../../components/device/nrf52_name_change.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdio.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdarg.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/types.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/types.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/stdio.h \
+ /usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdbool.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../config/sdk_config.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_error_weak.h \
+ ../../../../../../external/freertos/source/include/projdefs.h \
+ ../../../../../../external/freertos/source/include/portable.h \
+ ../../../../../../external/freertos/source/include/deprecated_definitions.h \
+ ../../../../../../external/freertos/portable/GCC/nrf52/portmacro.h \
+ ../../../../../../external/freertos/portable/CMSIS/nrf52/portmacro_cmsis.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../external/freertos/source/include/mpu_wrappers.h \
+ ../../../../../../external/freertos/source/include/task.h \
+ ../../../../../../external/freertos/source/include/list.h \
+ ../../../../../../external/freertos/source/include/queue.h \
+ ../../../../../../external/freertos/source/include/timers.h \
+ ../../../../../../external/freertos/source/include/task.h
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdlib.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/ieeefp.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_ansi.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/newlib.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_newlib_version.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/config.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/features.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stddef.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/reent.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/_ansi.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_types.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/_types.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/lock.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/cdefs.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/stdlib.h:
+
+../../../../../../external/freertos/source/include/FreeRTOS.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_stdint.h:
+
+../../../config/blinky_FreeRTOS_pca10040/FreeRTOSConfig.h:
+
+../../../../../../components/device/nrf.h:
+
+../../../../../../components/device/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../components/toolchain/system_nrf52.h:
+
+../../../../../../components/device/nrf52_bitfields.h:
+
+../../../../../../components/device/nrf51_to_nrf52.h:
+
+../../../../../../components/device/nrf52_name_change.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/nrf_assert.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/stdio.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdarg.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/types.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/machine/types.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include/sys/stdio.h:
+
+/usr/local/gcc-arm-none-eabi-6_2-2016q4/lib/gcc/arm-none-eabi/6.2.1/include/stdbool.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+
+../config/sdk_config.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/app_error_weak.h:
+
+../../../../../../external/freertos/source/include/projdefs.h:
+
+../../../../../../external/freertos/source/include/portable.h:
+
+../../../../../../external/freertos/source/include/deprecated_definitions.h:
+
+../../../../../../external/freertos/portable/GCC/nrf52/portmacro.h:
+
+../../../../../../external/freertos/portable/CMSIS/nrf52/portmacro_cmsis.h:
+
+../../../../../../components/libraries/util/app_util.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../external/freertos/source/include/mpu_wrappers.h:
+
+../../../../../../external/freertos/source/include/task.h:
+
+../../../../../../external/freertos/source/include/list.h:
+
+../../../../../../external/freertos/source/include/queue.h:
+
+../../../../../../external/freertos/source/include/timers.h:
+
+../../../../../../external/freertos/source/include/task.h:
